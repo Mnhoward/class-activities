f963052fdb03a44d87de976092ed51569505b5c9
Mnhoward <michelenhoward@gmail.com>
2021-03-13 14:12:47 -0500

    19

---
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,21 @@
+// Create a variable below
+
+var helloWorld = 'Hello World, I am here!';
+
+
+
+// Console.log() your variable here.
+console.log(helloWorld);
+
+alert(helloWorld);
+alert("Aloha Again!");
+
+var x = 3;
+var y = 2;
+var z = 'There are ' + (x + y) + ' coconuts on the tree';
+
+function addkids() {
+    console.log(z);
+}
+
+addkids();
